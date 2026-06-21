5e0c4f35221302928347348650d522097c287421
yruge <jasonjthen@gmail.com>
2026-06-21 15:15:49 +0700

    configure turbopack

--- gridism-portfolio/next.config.ts
+++ gridism-portfolio/next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: {
+    // Points Turbopack to the actual root of your project
+    root: '../',
+  },
 };
 
 export default nextConfig;
